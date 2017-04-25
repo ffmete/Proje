17b5d925a1dac864815110f2310282872c9e351a
ffmete <fatih.furkan.mete@hotmail.com>
2017-04-25 23:55:14 +0300

    proje

---
+++ spotify/src/test/java/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Mete on 25.04.2017.
+ */
+public class Test {
+    public static void main(String args []){
+
+        final Sarki sarki = new Sarki(1, "royals", 3,"https://www.youtube.com/watch?v=nlcIKh6sBtc");
+        System.out.println(sarki.Ad);
+
+
+
+    }
+
+}
